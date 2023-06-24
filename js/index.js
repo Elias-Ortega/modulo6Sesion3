a5946448059625480033448f4fb3a536337fd488
Elias Ortega <eliasortega2011@gmail.com>
2023-06-24 14:05:24 -0400

    ejemplo template literal, spread, funcion flecha

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
-
 const  { celular } = require('./celular.js');
 
-console.log(celular);
 
 
 
+
+const descripcionCelular = `El celular ${celular.marca} ${celular.modelo} es un celular del año ${celular.ano},
+                             color   ${celular.color} y su memoria es de ${celular.memoria}. 
+                             Registro con fecha: ${celular.fecha} ` ;
+
+console.log(descripcionCelular);
+
+const celularActualizado = { ...celular, marca:'iphone', modelo:'11'};
+console.log(celularActualizado);
+
+//Escribe un ejemplo de función de callback,pero esta vez utilizando la sintaxis de función de flecha.
+app.get('/', function(req,res){
+    res.send('status code 200');
+});
+
+app.get('/', (req,res) =>{
+    res.send('satus code 200');
+});
